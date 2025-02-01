7f187d47d21211e21586bd09e6038f2709a166e0
tobimadariola <tobimadariola@yahoo.com>
2025-02-01 05:35:13 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -96,7 +96,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.medium"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group_sonarqube.id]
-  key_name               = "kim"
+  key_name               = "Olamide"
   user_data = "${file("install_sonarqube.sh")}"
 
   tags = {
