072f164675c00a6359c6341c12afaf95cc2bdb00
tobimadariola <tobimadariola@yahoo.com>
2025-02-01 03:55:52 +0100

    newclone

--- JenkinsTFscript/ec2.tf
+++ JenkinsTFscript/ec2.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region    = "us-east-1"
-  profile   = "Abdulhakeem"
+  profile   = "MST"
 }
 
 
@@ -89,8 +89,8 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.small"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "kim"
-  user_data = "${file("install_jenkins.sh")}"
+  key_name               = "Olamide"
+  #user_data = "${file("install_jenkins.sh")}"
 
   tags = {
     Name = "jenkins_server"
@@ -98,33 +98,37 @@ resource "aws_instance" "ec2_instance" {
 }
 
 
-# an empty resource block
-#resource "null_resource" "name" {
+#an empty resource block
+resource "terraform_data" "name" {
 
-  # ssh into the ec2 instance 
-  #connection {
-   # type        = "ssh"
-    #user        = "ubuntu"
-    #private_key = file("~/Downloads/kim.pem")
-    #host        = aws_instance.ec2_instance.public_ip
+  #ssh into the ec2 instance 
+  connection {
+   type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/Downloads/Olamide.pem")
+    host        = aws_instance.ec2_instance.public_ip
+  }
   
 
-  # copy the install_jenkins.sh file from your computer to the ec2 instance 
-  #provisioner "file" {
-   # source      = "install_jenkins.sh"
-    #destination = "/tmp/install_jenkins.sh"
+  #copy the install_jenkins.sh file from your computer to the ec2 instance 
+  provisioner "file" {
+   source      = "install_jenkins.sh"
+    destination = "/tmp/install_jenkins.sh"
+  }
   
 
-  # set permissions and run the install_jenkins.sh file
-  #provisioner "remote-exec" {
-   # inline = [
-    #    "sudo chmod +x /tmp/install_jenkins.sh",
-     #   "sh /tmp/install_jenkins.sh",
-    
+  #set permissions and run the install_jenkins.sh file
+  provisioner "remote-exec" {
+   inline = [
+       "sudo chmod +x /tmp/install_jenkins.sh",
+       "sh /tmp/install_jenkins.sh", ]
+  }
   
 
-  # wait for ec2 to be created
-  #depends_on = [aws_instance.ec2_instance]
+  #wait for ec2 to be created
+  depends_on = [aws_instance.ec2_instance]
+
+}
 
 
 
